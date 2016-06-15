67f91bf1e6786dcdc635a3b4e0bcbfb213916ba6
kristjanjansen <kristjan.jansen@gmail.com>
2016-06-15 06:07:07 +0300

    cleanup

---
+++ app/Console/Commands/Source.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use ZipArchive;
+use GuzzleHttp\Client as Guzzle;
+use League\Csv\Reader;
+use Goutte\Client as Goutte;
+
+abstract class Source extends Command
+{
+
+    protected $csvreader;
+    protected $goutte;
+    protected $guzzle;
+    protected $ziparchive;
+
+    public function __construct()
+    {
+        
+        parent::__construct();
+
+        $this->goutte = new Goutte();
+        $this->guzzle = new Guzzle();
+        $this->ziparchive = new ZipArchive();
+
+    }
+
+    public function csv($csv)
+    {
+
+        return Reader::createFromString($csv);
+
+    }
+
+    public function cleanSource($sourcename)
+    {
+
+        $this->line("\nCleaning $sourcename previous data");
+
+        app('db')->table('source')->where('sourcename', '=', $sourcename)->delete();
+
+    }
+
+    public function fetchJson($sourceurl) {
+
+        $this->line('Downloading source');
+
+        $this->info($sourceurl);
+
+        return json_decode(file_get_contents($sourceurl));
+
+    }
+
+}
--- app/Console/Commands/SourceTripDestinations.php
+++ app/Console/Commands/SourceTripDestinations.php
@@ -4,43 +4,35 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class SourceTripDestinations extends Command
+class SourceTripDestinations extends Source
 {
 
     protected $signature = 'source:trip_destinations';
 
     public function handle()
     {
-        $sourcename = 'trip_destinations';
 
+        $sourcename = 'trip_destinations';
         $sourceurl = 'http://localhost:8000/api/destinations';
 
-        $this->line('Cleaning previous data');
+        $this->cleanSource($sourcename);
 
-        app('db')->table('source')->where('sourcename', '=', $sourcename)->delete();
-
-        $this->line('Downloading source');
-
-        $this->info($sourcename);
-
-        $data = json_decode(file_get_contents($sourceurl));
-
-        $this->line('Inserting data');
+        $data = $this->fetchJson($sourceurl);
 
         $this->output->progressStart(count((array)$data));
 
-        collect($data)->each(function($row) use ($sourcename) {
+            collect($data)->each(function($row) use ($sourcename) {
 
-            app('db')
-                ->table('source')
-                ->insert([
-                    'sourcename' => $sourcename,
-                    'value' => json_encode($row)
-                ]);
+                app('db')
+                    ->table('source')
+                    ->insert([
+                        'sourcename' => $sourcename,
+                        'value' => json_encode($row)
+                    ]);
 
-            $this->output->progressAdvance();
+                $this->output->progressAdvance();
 
-        });
+            });
 
         $this->output->progressFinish();
 
